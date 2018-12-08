c29f0932088287869af0e2c3cfb80f925537fa7a
yongming <zhuangyongming1900@gmail.com>
2018-12-08 18:03:57 +0800

    Trim the request path and log it on the server side

--- index.js
+++ index.js
@@ -2,10 +2,23 @@
 
 // dependencies
 const http = require('http');
+const url = require('url');
 
 // the server should respond to all reqruests with a string
 var server = http.createServer(function(req, res){
+
+    // Get the url and parse it 
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path (untrim the path by using pathname)
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Send the response
     res.end('hello world\n');
+
+    // Log the request path
+    console.log('Request is received on path: ' + trimmedPath);
 })
 
 // start the server, and have it listen on port 300
